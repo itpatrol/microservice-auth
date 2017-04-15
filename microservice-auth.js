57621b9d20c4bf0be0023fdbedc6800330c9621e
Gor Martsen <Gor.martsen@gmail.com>
2017-04-14 21:22:28 -0400

    Fix database auth settings

--- microservice-auth.js
+++ microservice-auth.js
@@ -16,7 +16,7 @@ var debug = {
 require('dotenv').config();
 
 var mservice = new Microservice({
-  mongoUrl: process.env.MONGO_URL,
+  mongoUrl: process.env.MONGO_URL + process.env.MONGO_PREFIX + process.env.MONGO_OPTIONS,
   mongoTable: process.env.MONGO_TABLE,
   secureKey: process.env.SECURE_KEY,
   schema: process.env.SCHEMA
